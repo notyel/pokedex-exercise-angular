99b1ebc8bb2fce678576cca5079199cb3a912808
Leyton Manuel Espitia Diaz <leyton_espitia@hotmail.com>
2023-09-22 01:35:52 -0500

    Migración a SCSS y ajustes en estilos CSS

--- pokedex-app/src/app/pokedex/pokedex.module.ts
+++ pokedex-app/src/app/pokedex/pokedex.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokedexRoutingModule } from './pokedex.routes';
-import { PokedexComponent } from './pokedex/pokedex.component';
 import { PokedexPageComponent } from './pages/pokedex-page/pokedex-page.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
@@ -10,7 +9,6 @@ import { PokemonComponent } from './pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    PokedexComponent,
     PokedexPageComponent,
     PokemonListComponent,
     PokemonComponent
